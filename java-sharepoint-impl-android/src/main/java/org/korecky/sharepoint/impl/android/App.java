d9326862dcc41173b152e4f408ffab55c4c087ff
vladislav@korecky.org <vladislav@korecky.org@fe30706b-22cb-a935-0df6-42dd41a24117>
2013-04-19 06:28:37 +0000

    Start of code refactoring to Abstract Factory Pattern.

---
+++ java-sharepoint-impl-android/src/main/java/org/korecky/sharepoint/impl/android/App.java
@@ -0,0 +1,13 @@
+package org.korecky.sharepoint.impl.android;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
