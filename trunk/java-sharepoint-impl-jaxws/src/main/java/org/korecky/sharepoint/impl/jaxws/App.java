95cc43540c169c89de1b199b47ac0b76de891b9b
vladislav@korecky.org <vladislav@korecky.org@fe30706b-22cb-a935-0df6-42dd41a24117>
2013-04-19 06:28:37 +0000

    Start of code refactoring to Abstract Factory Pattern.

---
+++ trunk/java-sharepoint-impl-jaxws/src/main/java/org/korecky/sharepoint/impl/jaxws/App.java
@@ -0,0 +1,13 @@
+package org.korecky.sharepoint.impl.jaxws;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
